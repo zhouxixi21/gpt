606462bf400829eef588a26a01ef561ffedecc82
xitongzhou <james1994zhou@hotmail.com>
2024-11-07 19:01:20 +0800

    update

--- service/Conversation_service.py
+++ service/Conversation_service.py
@@ -1,13 +1,17 @@
 import json
 import os
+import threading
 import uuid
 import requests
 from constant.Path_constant import Path_constant
 
 
+def request_develop(form):
+    requests.post('http://localhost:8000/develop', form)
+
+
 def send_email(message_list):
     file_path = Path_constant.NODE_CONVERSATION_FILE_PATH
-
     if os.path.exists(file_path):
         with open(Path_constant.NODE_CONVERSATION_FILE_PATH, 'r') as f:
             content = json.load(f)
@@ -31,4 +35,5 @@ def send_email(message_list):
         with open(file_path, "w") as file:
             json.dump(content, file, indent=4)
             file.close()
-        requests.post('http://localhost:8000/develop', json.dumps(content[len(content)-1]))
+        thread = threading.Thread(target=request_develop, args=(json.dumps(content[len(content) - 1], )))
+        thread.start()
