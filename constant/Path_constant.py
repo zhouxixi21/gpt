4ef97024961294b90c353d04e94ac5b48a60cdf7
xitongzhou <james1994zhou@hotmail.com>
2024-11-13 18:45:04 +0800

    update

--- constant/Path_constant.py
+++ constant/Path_constant.py
@@ -3,6 +3,6 @@ import os
 
 class Path_constant:
     NODE_CONVERSATION_FILE_PATH = '/Users/xitongzhou/Desktop/pythonProject/datagpt/log/message.json'
-    NODE_GPT_FILE_PATH = '/Users/xitongzhou/Desktop/pythonProject/datagpt/log/<placeholder>/gpt.log'
+    NODE_GPT_FILE_PATH = '/Users/xitongzhou/Desktop/pythonProject/execute-project/<placeholder>/gpt.log'
     GIT_TOKEN = ''
     GIT_ISSUE_PATH = '/Users/xitongzhou/Desktop/pythonProject/datagpt/log/git_issue.log'
